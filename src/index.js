2f6ba0491a990ecf56e169f2440e251e3c3de2cf
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-01-19 17:30:28 -0600

    prod build vs development build

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ heading.render();
 
 const helloWorldButton =new HelloWorldButton();
 helloWorldButton.render();
+
+if(process.env.NODE_ENV==='production'){
+    console.log(" production build");
+
+}
+else if(process.env.NODE_ENV==='development'){
+    console.log(" development mode build");
+
+}
+helloWorldButton.crazymethod();
 //addImage();
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         publicPath:''
     },
     
-    mode:'none',
+    mode:'development',
     module: {
         rules: [
             {
