0602cd078440d2c714044ce4b107715ae38c42bb
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-01-19 16:08:57 -0600

    extracting styles outside with mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path=require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -21,13 +21,13 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader'
                 ]
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader', 'css-loader', 'sass-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
                 ]
             },
             {
@@ -50,6 +50,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new UglifyJsPlugin()
+        new UglifyJsPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
     ]
 }
