d167560cb42324091fd42560a1115197f1f2afb0
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-01-19 14:50:28 -0600

    refering the url from website

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'./dist'),
-        publicPath:'dist/'
+        publicPath:'myAwesomesite/'
     },
     
     mode:'none',
