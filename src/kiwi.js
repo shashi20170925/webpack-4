5687a47c977f0f30cb2bee2fb67afd4925272282
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-01-19 18:21:14 -0600

    multiple entry files as multiple pages for server side rendering

---
+++ src/kiwi.js
@@ -0,0 +1,9 @@
+import Heading from './components/heading/heading';
+import KiwiImage from './components/kiwi-image/kiwi-image';
+
+const heading=new Heading();
+heading.render();
+
+
+const kiwiImage=new KiwiImage();
+kiwiImage.render();
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,9 +4,12 @@ const CleanWebpackPlugin=require('clean-webpack-plugin');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {'hello-world':'./src/hello-world.js',
+'kiwi': './src/kiwi.js'
+},
+    //'./src/index.js',
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname,'./dist'),
         publicPath:''
     },
@@ -60,7 +63,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: '[name].[contenthash].css'
         }),
         new CleanWebpackPlugin('dist'),
         new HtmlWebpackPlugin({
